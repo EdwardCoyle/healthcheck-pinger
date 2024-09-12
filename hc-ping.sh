cd7db09d1dea01aa59889bf2e939f5ced7b17029
Ed Coyle <edcoyle86@gmail.com>
2024-09-11 21:51:14 -0400

    Remove slug/etc

--- hc-ping.sh
+++ hc-ping.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-PING_KEY=fixme-your-ping-key-here
-
-# Use system's hostname as check's slug
-SLUG=$(hostname)
+PING_KEY=0e9ff055-6aae-45ef-8324-6220de84adce
 
 # Construct a ping URL and append "?create=1" at the end:
-URL=https://hc-ping.com/$PING_KEY/$SLUG?create=1
+URL=https://hc-ping.com/$PING_KEY
 
 # Send a ping:
 curl -m 10 --retry 5 $URL
